b98a360f6a3a0514bd722620692c8249fb53a346
DaviRdaSilva <Davi.rsilva@sptech.school>
2023-05-15 14:59:09 -0300

    Conectando com o Banco, e Chaves Estrageniras Acertadas

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ const SERIAL_BAUD_RATE = 9600;
 const SERVIDOR_PORTA = 3000;
 const HABILITAR_OPERACAO_INSERIR = true;
 
+var idLimite = 1;
+var idLocal = 1;
+var contador = 1;
+
 const serial = async (
     valoresDht11Umidade,
     valoresMediaTemperatura
@@ -14,8 +18,8 @@ const serial = async (
         {
             host: 'localhost',
             port: 3306,
-            user: 'aluno',
-            password: 'sptech',
+            user: 'User_DataQuino',
+            password: '@secureSHIP',
             database: 'Secure_Ship'
         }
     ).promise();
@@ -47,18 +51,31 @@ const serial = async (
         // valoresChave.push(chave);
 
         if (HABILITAR_OPERACAO_INSERIR) {
+            contador++;
             await poolBancoDados.execute(
-                'INSERT INTO Leitura (Leitura, FKSensor_LE) VALUES (?, ?)',
-                [dht11Umidade, 1],
+                'INSERT INTO Leitura (Leitura_temp, Leitura_Umi, FKLocal_LE, FKLimite_LE) VALUES (?,?,?,?)',
+                [mediaTemp, dht11Umidade, idLocal, idLimite],
             );
-        }
 
-        if (HABILITAR_OPERACAO_INSERIR){
-            await poolBancoDados.execute(
-                'INSERT INTO Leitura (Leitura, FKSensor_LE) VALUES (?, ?)',
-                [mediaTemp, 2]
-            );
+            if (contador % 2 == 0){
+                idLimite = 2;
+            }else {
+                idLimite = 1;
+            }
+
+            if (contador <= 10 ) {
+                idLocal = 1;
+            }else if (contador <= 20) {
+                idLocal = 2;
+            }else if (contador <= 30) {
+                idLocal = 3;
+                
+            }else if (contador <= 40) {
+                idLocal = 4;
+            }
+            
         }
+           
 
     });
     arduino.on('error', (mensagem) => {
@@ -69,8 +86,6 @@ const serial = async (
 const servidor = (
     valoresDht11Umidade,
     valoresMediaTemperatura,
-    // valoresLuminosidade,
-    // valoresChave
 ) => {
     const app = express();
     app.use((request, response, next) => {
@@ -87,30 +102,17 @@ const servidor = (
     app.get('/sensores/MediaTemperatura', (_, response) => {
         return response.json(valoresMediaTemperatura);
     });
-
-    // app.get('/sensores/luminosidade', (_, response) => {
-    //     return response.json(valoresLuminosidade);
-    // });
-    // app.get('/sensores/chave', (_, response) => {
-    //     return response.json(valoresChave);
-    // });
 }
 
 (async () => {
     const valoresDht11Umidade = [];
     const valoresMediaTemperatura = [];
-    // const valoresLuminosidade = [];
-    // const valoresChave = [];
     await serial(
         valoresDht11Umidade,
         valoresMediaTemperatura
-        // valoresLuminosidade,
-        // valoresChave
     );
     servidor(
         valoresDht11Umidade,
         valoresMediaTemperatura
-        // valoresLuminosidade,
-        // valoresChave
     );
 })();
